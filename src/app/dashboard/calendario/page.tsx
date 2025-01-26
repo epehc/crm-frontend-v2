2d7e7dc6c6df5ecf169e5102e205806add639cde
Jose Cardona <jpcardonamena@gmail.com>
2025-01-26 14:27:05 +0100

    added the iframe from the Google Calendar

---
+++ src/app/dashboard/calendario/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <iframe src="https://calendar.google.com/calendar/embed?src=c_d0ff24e3375411740ea1909fbf3519887d58a107123a806b88ad6997e21792b5%40group.calendar.google.com&ctz=America%2FGuatemala&mode=WEEK"
+                style={{display:'flex', width:'100%', height:'100%', border: 0}}  frameBorder="0" scrolling="no">
+        </iframe>
+    );
+}
