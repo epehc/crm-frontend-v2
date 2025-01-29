b599eb05877bd62393d24d65e185b804293db0f2
Jose Cardona <jpcardonamena@gmail.com>
2025-01-29 19:36:48 +0100

    Changed it so that the name of the candidate would be bigger, and added action buttons (Generar informe pending)

--- src/components/ui/candidato-id/candidato-info.tsx
+++ src/components/ui/candidato-id/candidato-info.tsx
@@ -1,7 +1,8 @@
 import {Button} from "@/components/ui/button";
-import {CalendarIcon, DocumentPlusIcon} from "@heroicons/react/24/outline";
+import {DocumentPlusIcon} from "@heroicons/react/24/outline";
 import {Candidato} from "@/lib/definitions";
 import {Separator} from "@/components/ui/separator";
+import AgendarEntrevistaDialog from "@/components/ui/candidato-id/agendar-entrevista-dialog";
 
 
 
@@ -14,22 +15,26 @@ export default function CandidatoInfo({ candidato }: { candidato: Candidato | nu
     return (
         <div className="flex flex-col space-y-4">
             <div className="w-4/5 flex flex-col space-y-4">
-                <div className="grid grid-cols-4 gap-4">
+                <p className='text-3xl'><strong> {candidato.nombre}</strong></p>
+                <div className="grid grid-cols-3 gap-4">
                     <div>
-                        <p><strong>Nombre:</strong> {candidato.nombre}</p>
                         <p><strong>Género:</strong> {candidato.genero}</p>
                         <p><strong>Puesto Aplicado:</strong> {candidato.puesto_aplicado}</p>
+                        <p><strong>Cómo se Enteró:</strong> {candidato.como_se_entero}</p>
+                        <p><strong>Aspiración Salarial:</strong> {candidato.aspiracion_salarial}</p>
                     </div>
                     <div>
                         <p><strong>Teléfono WhatsApp:</strong> {candidato.telefono_whatsapp}</p>
                         <p><strong>Teléfono:</strong> {candidato.telefono}</p>
                         <p><strong>Correo:</strong> {candidato.correo}</p>
                     </div>
-                    <div>
-                        <p><strong>Cómo se Enteró:</strong> {candidato.como_se_entero}</p>
-                        <p><strong>Aspiración Salarial:</strong> {candidato.aspiracion_salarial}</p>
+                    <div className='grid grid-cols-1 gap-4 w-[240px]'>
+                        <AgendarEntrevistaDialog candidato={candidato}/>
+                        <Button>
+                            <DocumentPlusIcon/>
+                            Generar informe
+                        </Button>
                     </div>
-
                 </div>
             </div>
             <Separator/>
