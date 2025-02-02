e2d28d318f2e83ed8b35ea515bc9edd0a406bb8f
Jose Cardona <jpcardonamena@gmail.com>
2025-02-02 19:54:53 +0100

    fixed styling for form

--- src/components/ui/dashboard/sidenav.tsx
+++ src/components/ui/dashboard/sidenav.tsx
@@ -15,8 +15,8 @@ export default function SideNav() {
     return(
         <div className="flex h-full flex-col px-3 py-4 md:px-2">
             <Link
-                className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-                href="/dashboard"
+                className="mb-2 flex h-20 items-end justify-start rounded-md bg-black p-4 md:h-40"
+                href="/dashboard/candidatos"
             >
                 <div className="w-32 text-white md:w-40">
                     <b>PROreclutamiento CRM</b>
@@ -26,7 +26,7 @@ export default function SideNav() {
                 <NavLinks />
                 <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
                 <form>
-                    <button onClick={handleSignOut} className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                    <button onClick={handleSignOut} className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-gray-300 md:flex-none md:justify-start md:p-2 md:px-3">
                         <PowerIcon className="w-6" />
                         <div className="hidden md:block">Cerrar Sesion</div>
                     </button>
