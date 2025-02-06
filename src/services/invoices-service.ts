ec0deef2df68e200ad7fbc37767e86518c9a429c
Jose Cardona <jpcardonamena@gmail.com>
2025-02-06 19:29:52 +0100

    changed api methods since during development they appeared to not work

--- src/services/invoices-service.ts
+++ src/services/invoices-service.ts
@@ -1,6 +1,21 @@
+import { Factura, NuevaFactura, NuevoPago } from '@/lib/definitions';
 import api from './api';
 
-export const getAllFacturas = async (page: number, pageSize: number, token: string) => {
+export const getAllFacturas = async (token: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/facturas/all`,{
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    if (!response.ok) {
+        throw new Error(`Failed to fetch invoices: ${response.statusText}`);
+    }
+    return response.json();
+}
+
+
+export const getFacturas = async (page: number, pageSize: number, token: string) => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/facturas?page=${page}&pageSize=${pageSize}`,{
         headers: {
             'Authorization': `Bearer ${token}`,
@@ -18,9 +33,17 @@ export const getFacturaByFacturaId = async (factura_id: string) => {
     return response.data;
 }
 
-export const getFacturasByClienteId = async (cliente_id: string) => {
-    const response = await api.get(`/facturas/cliente/${cliente_id}`);
-    return response.data;
+export const getFacturasByClienteId = async (client_id: string, token: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/facturas/cliente/${client_id}`,{
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    if (!response.ok) {
+        throw new Error(`Failed to fetch invoices: ${response.statusText}`);
+    }
+    return response.json();
 }
 
 export const getFacturasByNit = async (nit: string) => {
@@ -28,14 +51,35 @@ export const getFacturasByNit = async (nit: string) => {
     return response.data;
 }
 
-export const createFactura = async (factura: any) => {
-    const response = await api.post('/facturas', factura);
-    return response.data;
+export const createFactura = async (factura: NuevaFactura, token: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/facturas`, {
+        method: "POST",
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(factura),
+    })
+    if (!response.ok) {
+        console.log("Failed to create invoice: ", response.statusText);
+        throw new Error(`Failed to create invoice: ${response.statusText}`);
+    }
+    return response.json();
 }
 
-export const updateFactura = async (factura_id: string, factura: any) => {
-    const response = await api.put(`/facturas/${factura_id}`, factura);
-    return response.data;
+export const updateFactura = async (factura: Factura, token: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/facturas/${factura.factura_id}`, {
+        method: "PUT",
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(factura),
+    })
+    if (!response.ok) {
+        throw new Error(`Failed to update invoice: ${response.statusText}`);
+    }
+    return response.json();
 }
 
 export const deleteFactura = async (factura_id: string) => {
@@ -48,11 +92,33 @@ export const getLatestFacturas = async () => {
     return response.data;
 }
 
+export const anularFactura = async (factura_id: number, token: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/facturas/anular-factura/${factura_id}`, {
+        method: "PUT",
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    if (!response.ok) {
+        throw new Error(`Failed to update invoice: ${response.statusText}`);
+    }
+    return response.json();
+}
+
 //***************************************************************
 
-export const getAllPagos = async () => {
-    const response = await api.get('/pagos');
-    return response.data;
+export const getAllPagos = async (token: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/pagos/`,{
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    if (!response.ok) {
+        throw new Error(`Failed to fetch payments: ${response.statusText}`);
+    }
+    return response.json();
 }
 
 export const getPagoByPagoId = async (pago_id: string) => {
@@ -60,14 +126,32 @@ export const getPagoByPagoId = async (pago_id: string) => {
     return response.data;
 }
 
-export const getPagosByFacturaId = async (factura_id: string) => {
-    const response = await api.get(`/pagos/factura/${factura_id}`);
-    return response.data;
+export const getPagosByFacturaId = async (factura_id: number, token: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/pagos/factura/${factura_id}`,{
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    if (!response.ok) {
+        throw new Error(`Failed to fetch payments: ${response.statusText}`);
+    }
+    return response.json();
 }
 
-export const createPago = async (pago: any) => {
-    const response = await api.post('/pagos', pago);
-    return response.data;
+export const createPago = async (nuevoPago: NuevoPago, token: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INVOICES_API_URL}/pagos`, {
+        method: "POST",
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(nuevoPago),
+    })
+    if (!response.ok) {
+        throw new Error(`Failed to create payment: ${response.statusText}`);
+    }
+    return response.json();
 }
 
 export const updatePago = async (pago_id: string, pago: any) => {
