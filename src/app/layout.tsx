43be058d7a9827c7e7101352d525f1e2897e655b
Jose Cardona <jpcardonamena@gmail.com>
2025-02-09 15:19:18 +0100

    Added Toaster to display notifications as toasts instead of alerts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionProviderWrapper from './SessionProviderWrapper';
-
+import { Toaster } from "@/components/ui/toaster"
 
 
 const geistSans = Geist({
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       <SessionProviderWrapper>
           {children}
+          <Toaster />
       </SessionProviderWrapper>
       </body>
     </html>
